7f53cd228303faf4e1b6f09971bd69ead360cef7
Sophie Williams <sophia.williams@duke.edu>
2021-11-20 12:36:39 -0500

    almost done

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -83,6 +83,15 @@ public class HuffProcessor {
 		String[] encodings = new String[ALPH_SIZE+1];
 		makeEncodings(root,"",encodings);
 
+		while(true){
+			int val=in.readBits(BITS_PER_WORD);
+			if (val ==-1) break;
+			String code=encodings[val];
+			out.writeBits(code.length(),Integer.parseInt(code,2));
+		}
+		String code = encodings[PSEUDO_EOF];
+		out.writeBits(code.length(), Integer.parseInt(code,2));
+
 		out.close();
 	}
 
